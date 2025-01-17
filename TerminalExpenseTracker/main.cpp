7d3c08b5ebab61ad036c681dfecdc6a37b8d3cab
Fudhail Patel <fuddrizzy@gmail.com>
2025-01-17 18:09:57 +0200

    Initial Commit

---
+++ TerminalExpenseTracker/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TerminalExpenseTracker
+//
+//  Created by Fudhail Patel on 1/17/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
